de8b0b1ceeb7f78eecb588bb105d8c3e2a8a4934
yungez <yungez@microsoft.com>
2017-09-18 14:54:30 +0800

    initial checkin

---
+++ samplecode/src/main/java/com/microsoft/azure/sample/User.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+
+package com.microsoft.azure.sample;
+
+import com.microsoft.azure.spring.data.cosmosdb.documentdb.core.mapping.Document;
+import com.microsoft.azure.spring.data.cosmosdb.documentdb.core.mapping.PartitionKey;
+import org.springframework.data.annotation.Id;
+
+@Document(collection = "mycollection")
+public class User {
+    @Id
+    private String emailAddress;
+    private String firstName;
+    @PartitionKey
+    private String lastName;
+
+    public User(String emailAddress, String firstName, String lastName) {
+        this.emailAddress = emailAddress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s %s", emailAddress, firstName, lastName);
+    }
+}
+
---
+++ src/test/java/com/microsoft/azure/spring/data/cosmosdb/documentdb/domain/Address.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.data.cosmosdb.documentdb.domain;
+
+import com.microsoft.azure.spring.data.cosmosdb.documentdb.core.mapping.Document;
+import com.microsoft.azure.spring.data.cosmosdb.documentdb.core.mapping.PartitionKey;
+import org.springframework.data.annotation.Id;
+
+@Document(ru = "1000")
+public class Address {
+    @Id
+    String postalCode;
+    String street;
+    @PartitionKey
+    String city;
+
+    public Address(String postalCode, String city, String street) {
+        this.postalCode = postalCode;
+        this.city = city;
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String code) {
+        this.postalCode = code;
+    }
+}
