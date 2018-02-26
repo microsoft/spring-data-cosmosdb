de7076b777bcc8ef318ef02d30a444fc7d332614
weiping <wepa@microsoft.com>
2018-02-26 16:09:10 +0800

    split test into partition and non-partition

--- src/test/java/com/microsoft/azure/spring/data/documentdb/core/DocumentDbTemplatePartitionIT.java
+++ src/test/java/com/microsoft/azure/spring/data/documentdb/core/DocumentDbTemplatePartitionIT.java
@@ -9,9 +9,12 @@ package com.microsoft.azure.spring.data.documentdb.core;
 import com.microsoft.azure.documentdb.ConnectionPolicy;
 import com.microsoft.azure.documentdb.ConsistencyLevel;
 import com.microsoft.azure.documentdb.DocumentClient;
+import com.microsoft.azure.documentdb.PartitionKey;
 import com.microsoft.azure.spring.data.documentdb.Constants;
 import com.microsoft.azure.spring.data.documentdb.core.convert.MappingDocumentDbConverter;
 import com.microsoft.azure.spring.data.documentdb.core.mapping.DocumentDbMappingContext;
+import com.microsoft.azure.spring.data.documentdb.core.query.Criteria;
+import com.microsoft.azure.spring.data.documentdb.core.query.Query;
 import com.microsoft.azure.spring.data.documentdb.domain.Address;
 import com.microsoft.azure.spring.data.documentdb.domain.Person;
 import org.junit.After;
@@ -34,7 +37,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @PropertySource(value = {"classpath:application.properties"})
-public class DocumentDbTemplateIT {
+public class DocumentDbTemplatePartitionIT {
     private static final String TEST_ID = "template_it_id";
     private static final String TEST_NOTEXIST_ID = "non_exist_id";
 
@@ -42,6 +45,8 @@ public class DocumentDbTemplateIT {
     private static final List<String> HOBBIES = Constants.HOBBIES;
     private static final List<Address> ADDRESSES = Constants.ADDRESSES;
     private static final Person TEST_PERSON = new Person(TEST_ID, "testfirstname", "testlastname", HOBBIES, ADDRESSES);
+
+    private static final String PARTITION_KEY = "lastName";
 
     @Value("${documentdb.uri}")
     private String documentDbUri;
@@ -73,8 +78,8 @@ public class DocumentDbTemplateIT {
 
         dbTemplate = new DocumentDbTemplate(documentClient, dbConverter, TEST_DB_NAME);
 
-        dbTemplate.createCollectionIfNotExists(Person.class.getSimpleName(), null, null);
-        dbTemplate.insert(Person.class.getSimpleName(), TEST_PERSON, null);
+        dbTemplate.createCollectionIfNotExists(Person.class.getSimpleName(), PARTITION_KEY, 1000);
+        dbTemplate.insert(Person.class.getSimpleName(), TEST_PERSON, new PartitionKey(TEST_PERSON.getLastName()));
     }
 
     @After
@@ -82,64 +87,83 @@ public class DocumentDbTemplateIT {
         dbTemplate.deleteAll(Person.class.getSimpleName());
     }
 
-    @Test(expected = RuntimeException.class)
-    public void testInsertDuplicateId() {
-        dbTemplate.insert(Person.class.getSimpleName(), TEST_PERSON, null);
-    }
-
     @Test
-    public void testFindAll() {
-        final List<Person> result = dbTemplate.findAll(Person.class.getSimpleName(), Person.class);
+    public void testFindAllByPartition() {
+        final Criteria criteria = new Criteria("lastName");
+        criteria.is(TEST_PERSON.getLastName());
+        final Query query = new Query(criteria);
+
+        final List<Person> result = dbTemplate.find(query, Person.class, Person.class.getSimpleName());
         assertThat(result.size()).isEqualTo(1);
-        assertThat(result.get(0)).isEqualTo(TEST_PERSON);
+        assertTrue(result.get(0).equals(TEST_PERSON));
     }
 
     @Test
-    public void testFindById() {
-        final Person result = dbTemplate.findById(Person.class.getSimpleName(),
-                TEST_PERSON.getId(), Person.class);
-        assertTrue(result.equals(TEST_PERSON));
+    public void testFindByIdWithPartition() {
+        final Criteria criteria = new Criteria("id");
+        criteria.is(TEST_PERSON.getId());
+        criteria.and("lastName").is(TEST_PERSON.getLastName());
+        final Query query = new Query(criteria);
 
-        final Person nullResult = dbTemplate.findById(Person.class.getSimpleName(),
-                TEST_NOTEXIST_ID, Person.class);
-        assertThat(nullResult).isNull();
+        final List<Person> result = dbTemplate.find(query, Person.class, Person.class.getSimpleName());
+        assertThat(result.size()).isEqualTo(1);
+        assertTrue(result.get(0).equals(TEST_PERSON));
     }
 
     @Test
-    public void testUpsertNewDocument() {
-        // Delete first as was inserted in setup
-        dbTemplate.deleteById(Person.class.getSimpleName(), TEST_PERSON.getId(), Person.class, null);
+    public void testFindByNonExistIdWithPartition() {
+        final Criteria criteria = new Criteria("id");
+        criteria.is(TEST_NOTEXIST_ID);
+        criteria.and("lastName").is(TEST_PERSON.getLastName());
+        final Query query = new Query(criteria);
 
+        final List<Person> result = dbTemplate.find(query, Person.class, Person.class.getSimpleName());
+        assertThat(result.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testUpsertNewDocumentPartition() {
         final String firstName = "newFirstName_" + UUID.randomUUID().toString();
         final Person newPerson = new Person(null, firstName, "newLastName", null, null);
 
-        dbTemplate.upsert(Person.class.getSimpleName(), newPerson, null, null);
+        final String partitionKeyValue = newPerson.getLastName();
+        dbTemplate.upsert(Person.class.getSimpleName(), newPerson, null, new PartitionKey(partitionKeyValue));
 
         final List<Person> result = dbTemplate.findAll(Person.class);
 
-        assertThat(result.size()).isEqualTo(1);
-        assertTrue(result.get(0).getFirstName().equals(firstName));
+        assertThat(result.size()).isEqualTo(2);
+
+        final Person person = result.stream()
+                .filter(p -> p.getLastName().equals(partitionKeyValue)).findFirst().get();
+        assertThat(person.getFirstName()).isEqualTo(firstName);
     }
 
     @Test
-    public void testUpdate() {
+    public void testUpdatePartition() {
         final Person updated = new Person(TEST_PERSON.getId(), "updatedname",
                 TEST_PERSON.getLastName(), TEST_PERSON.getHobbies(), TEST_PERSON.getShippingAddresses());
-        dbTemplate.upsert(Person.class.getSimpleName(), updated, updated.getId(), null);
+        dbTemplate.upsert(Person.class.getSimpleName(), updated, updated.getId(),
+                new PartitionKey(updated.getLastName()));
 
-        final Person result = dbTemplate.findById(Person.class.getSimpleName(),
-                updated.getId(), Person.class);
+        final List<Person> result = dbTemplate.findAll(Person.class);
+        final Person person = result.stream().filter(p -> TEST_PERSON.getId().equals(p.getId())).findFirst().get();
 
-        assertTrue(result.equals(updated));
+        assertTrue(person.equals(updated));
     }
 
     @Test
-    public void testDeleteById() {
-        final Person person2 = new Person("newid", "newfn", "newln", HOBBIES, ADDRESSES);
-        dbTemplate.insert(person2, null);
-        assertThat(dbTemplate.findAll(Person.class).size()).isEqualTo(2);
+    public void testDeleteByIdPartition() {
+        // insert new document with same partition key
+        final Person person2 = new Person("newid", "newfn", TEST_PERSON.getLastName(), HOBBIES, ADDRESSES);
+        dbTemplate.insert(Person.class.getSimpleName(), person2, new PartitionKey(person2.getLastName()));
 
-        dbTemplate.deleteById(Person.class.getSimpleName(), TEST_PERSON.getId(), null, null);
+        final List<Person> inserted = dbTemplate.findAll(Person.class);
+        assertThat(inserted.size()).isEqualTo(2);
+        assertThat(inserted.get(0).getLastName()).isEqualTo(TEST_PERSON.getLastName());
+        assertThat(inserted.get(1).getLastName()).isEqualTo(TEST_PERSON.getLastName());
+
+        dbTemplate.deleteById(Person.class.getSimpleName(),
+                TEST_PERSON.getId(), Person.class, new PartitionKey(TEST_PERSON.getLastName()));
 
         final List<Person> result = dbTemplate.findAll(Person.class);
         assertThat(result.size()).isEqualTo(1);
