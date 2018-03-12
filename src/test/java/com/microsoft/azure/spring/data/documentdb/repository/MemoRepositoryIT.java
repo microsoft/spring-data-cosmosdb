b8d93feb7557855fce64c280d073cba958ccf4b9
weiping <wepa@microsoft.com>
2018-03-12 10:06:31 +0800

    refactor variable name as constant and init in beforeClass

--- src/test/java/com/microsoft/azure/spring/data/documentdb/repository/MemoRepositoryIT.java
+++ src/test/java/com/microsoft/azure/spring/data/documentdb/repository/MemoRepositoryIT.java
@@ -9,6 +9,7 @@ import com.microsoft.azure.spring.data.documentdb.TestUtils;
 import com.microsoft.azure.spring.data.documentdb.domain.Memo;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,30 +30,32 @@ public class MemoRepositoryIT {
 
     private static final String ID_1 = "id_1";
     private static final String MESSAGE_1 = "first message";
-    private static final String DATE_1 = "1/1/2000";
-
+    private static final String DATE_STR_1 = "1/1/2000";
 
     private static final String ID_2 = "id_2";
     private static final String MESSAGE_2 = "second message";
-    private static final String DATE_2 = "1/1/2001";
+    private static final String DATE_STR_2 = "1/1/2001";
 
-    private Date date1;
-    private Date date2;
-    private Memo testMemo1;
-    private Memo testMemo2;
+    private static Date DATE_1;
+    private static Date DATE_2;
+    private static Memo TEST_MEMO_1;
+    private static Memo TEST_MEMO_2;
 
     @Autowired
     MemoRepository repository;
 
-    @Before
-    public void setup() throws ParseException{
-        date1 = DATE_FORMAT.parse(DATE_1);
-        date2 = DATE_FORMAT.parse(DATE_2);
-        testMemo1 = new Memo(ID_1, MESSAGE_1, date1);
-        testMemo2 = new Memo(ID_2, MESSAGE_2, date2);
+    @BeforeClass
+    public static void init() throws ParseException {
+        DATE_1 = DATE_FORMAT.parse(DATE_STR_1);
+        DATE_2 = DATE_FORMAT.parse(DATE_STR_2);
+        TEST_MEMO_1 = new Memo(ID_1, MESSAGE_1, DATE_1);
+        TEST_MEMO_2 = new Memo(ID_2, MESSAGE_2, DATE_2);
+    }
 
-        repository.save(testMemo1);
-        repository.save(testMemo2);
+    @Before
+    public void setup() {
+        repository.save(TEST_MEMO_1);
+        repository.save(TEST_MEMO_2);
     }
 
     @After
@@ -69,11 +72,11 @@ public class MemoRepositoryIT {
 
     @Test
     public void testFindByDate() throws ParseException {
-        final List<Memo> result = repository.findMemoByDate(DATE_FORMAT.parse(DATE_1));
+        final List<Memo> result = repository.findMemoByDate(DATE_FORMAT.parse(DATE_STR_1));
 
         assertThat(result.size()).isEqualTo(1);
         assertThat(result.get(0).getId().equals(ID_1));
         assertThat(result.get(0).getMessage().equals(MESSAGE_1));
-        assertThat(result.get(0).getDate().equals(date1));
+        assertThat(result.get(0).getDate().equals(DATE_1));
     }
 }
