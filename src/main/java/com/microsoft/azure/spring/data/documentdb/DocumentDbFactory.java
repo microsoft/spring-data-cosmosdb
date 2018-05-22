b64de55d690c3c457926baa1edddd2a2e4a2665e
Pan Li <panli@microsoft.com>
2018-05-22 11:33:50 +0800

    Add ApplicationInsights to cosmosdb factory.
    * Remove old UA.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/spring/data/documentdb/DocumentDbFactory.java
+++ src/main/java/com/microsoft/azure/spring/data/documentdb/DocumentDbFactory.java
@@ -9,40 +9,53 @@ package com.microsoft.azure.spring.data.documentdb;
 import com.microsoft.azure.documentdb.ConnectionPolicy;
 import com.microsoft.azure.documentdb.ConsistencyLevel;
 import com.microsoft.azure.documentdb.DocumentClient;
-import com.microsoft.azure.spring.data.documentdb.common.GetHashMac;
-import com.microsoft.azure.spring.data.documentdb.common.PropertyLoader;
+import com.microsoft.azure.spring.data.documentdb.common.TelemetryProperties;
+import com.microsoft.azure.spring.data.documentdb.common.TelemetryProxy;
 import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DocumentDbFactory {
 
-    private static final String USER_AGENT_SUFFIX = Constants.USER_AGENT_SUFFIX + PropertyLoader.getProjectVersion();
-
     private DocumentClient documentClient;
+    private final TelemetryProxy telemetryProxy;
 
     public DocumentDbFactory(String host, String key) {
-        this(host, key, true);
-    }
-
-    public DocumentDbFactory(String host, String key, boolean isBiEnabled) {
         Assert.hasText(host, "host must not be empty!");
         Assert.hasText(key, "key must not be empty!");
 
         final ConnectionPolicy policy = ConnectionPolicy.GetDefault();
 
-        String userAgent = ";" + USER_AGENT_SUFFIX;
-        if (isBiEnabled && GetHashMac.getHashMac() != null) {
-            userAgent += ";" + GetHashMac.getHashMac();
-        }
-        policy.setUserAgentSuffix(userAgent);
+        this.documentClient = new DocumentClient(host, key, policy, ConsistencyLevel.Session);
+        this.telemetryProxy = new TelemetryProxy(this.isTelemetryAllowed());
 
-        documentClient = new DocumentClient(host, key, policy, ConsistencyLevel.Session);
+        this.trackCustomEvent();
     }
 
     public DocumentDbFactory(DocumentClient client) {
         this.documentClient = client;
+        this.telemetryProxy = new TelemetryProxy(this.isTelemetryAllowed());
+
+        this.trackCustomEvent();
+    }
+
+    private boolean isTelemetryAllowed() {
+        // panli: will allow user to disable this from application.properties in another PR.
+        return true;
     }
 
     public DocumentClient getDocumentClient() {
         return documentClient;
     }
+
+    private void trackCustomEvent() {
+        final Map<String, String> customProperties = new HashMap<>();
+
+        customProperties.put(TelemetryProperties.PROPERTY_SERVICE_NAME, "cosmosdb");
+
+        this.telemetryProxy.trackEvent(ClassUtils.getUserClass(this.getClass()).getSimpleName(), customProperties);
+    }
 }
+
