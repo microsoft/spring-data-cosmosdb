92918746505d2b5f28385358384b2f25be62264d
Pan Li <panli@microsoft.com>
2018-08-13 10:08:13 +0800

    Remove generator from DocumentQuery.
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/spring/data/cosmosdb/core/query/DocumentQuery.java
+++ src/main/java/com/microsoft/azure/spring/data/cosmosdb/core/query/DocumentQuery.java
@@ -5,13 +5,9 @@
  */
 package com.microsoft.azure.spring.data.cosmosdb.core.query;
 
-import com.microsoft.azure.documentdb.SqlQuerySpec;
 import com.microsoft.azure.spring.data.cosmosdb.core.criteria.Criteria;
-import com.microsoft.azure.spring.data.cosmosdb.core.generator.QuerySpecGenerator;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.lang.NonNull;
-import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -22,17 +18,8 @@ public class DocumentQuery {
     @Getter
     private final Criteria criteria;
 
-    @Setter
-    private QuerySpecGenerator generator = null;
-
     public DocumentQuery(@NonNull Criteria criteria) {
         this.criteria = criteria;
-    }
-
-    public SqlQuerySpec sqlQuerySpecGenerate() {
-        Assert.notNull(this.generator, "Generator should not be null");
-
-        return this.generator.generate(this);
     }
 
     private Optional<Criteria> getSubjectCriteriaDfs(@NonNull Criteria criteria, @NonNull String keyName) {
