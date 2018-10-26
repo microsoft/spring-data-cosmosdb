bbb7d8f5b08bcf3dc9dbc74d7ea017c2904b99c8
Jan-Hendrik Telke <jan.hendrik.telke@gmail.com>
2018-10-26 08:32:23 +0200

    Java zoned date time support (#264)
    * added Java ZonedDateTimeSupport
    
    * fixed checkstyle error/line longer than 120 characters
    
    * implemented ZonedDateTime read support
    
    * Resolve comments.
    
    Signed-off-by: Pan Li <panli@microsoft.com>

--- src/main/java/com/microsoft/azure/spring/data/cosmosdb/core/convert/MappingDocumentDbConverter.java
+++ src/main/java/com/microsoft/azure/spring/data/cosmosdb/core/convert/MappingDocumentDbConverter.java
@@ -8,6 +8,7 @@ package com.microsoft.azure.spring.data.cosmosdb.core.convert;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.microsoft.azure.documentdb.Document;
 import com.microsoft.azure.spring.data.cosmosdb.Constants;
 import com.microsoft.azure.spring.data.cosmosdb.core.mapping.DocumentDbPersistentEntity;
@@ -20,14 +21,18 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.data.convert.EntityConverter;
+import org.springframework.data.mapping.MappingException;
 import org.springframework.data.mapping.PersistentPropertyAccessor;
 import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.mapping.model.ConvertingPropertyAccessor;
-import org.springframework.data.mapping.MappingException;
 import org.springframework.util.Assert;
 
 import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+
+import static com.microsoft.azure.spring.data.cosmosdb.Constants.ISO_8601_COMPATIBLE_DATE_PATTERN;
 
 public class MappingDocumentDbConverter
         implements EntityConverter<DocumentDbPersistentEntity<?>, DocumentDbPersistentProperty, Object, Document>,
@@ -62,6 +67,8 @@ public class MappingDocumentDbConverter
     protected <R extends Object> R readInternal(final DocumentDbPersistentEntity<?> entity, Class<R> type,
                                                 final Document sourceDocument) {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(provideAdvancedSerializersModule());
+
         try {
             final DocumentDbPersistentProperty idProperty = entity.getIdProperty();
             final Object idValue = sourceDocument.getId();
@@ -75,9 +82,15 @@ public class MappingDocumentDbConverter
 
             return objectMapper.readValue(jsonObject.toString(), type);
         } catch (IOException e) {
-            throw  new IllegalStateException("Failed to read the source document " + sourceDocument.toJson()
+            throw new IllegalStateException("Failed to read the source document " + sourceDocument.toJson()
                     + "  to target type " + type, e);
         }
+    }
+
+    private SimpleModule provideAdvancedSerializersModule() {
+        final SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addDeserializer(ZonedDateTime.class, new ZonedDateTimeDeserializer());
+        return simpleModule;
     }
 
     @Override
@@ -146,6 +159,7 @@ public class MappingDocumentDbConverter
 
     /**
      * Convert a property value to the value stored in CosmosDB
+     *
      * @param fromPropertyValue
      * @return
      */
@@ -157,6 +171,9 @@ public class MappingDocumentDbConverter
         // com.microsoft.azure.documentdb.JsonSerializable#set(String, T) cannot set values for Date and Enum correctly
         if (fromPropertyValue instanceof Date) {
             fromPropertyValue = ((Date) fromPropertyValue).getTime();
+        } else if (fromPropertyValue instanceof ZonedDateTime) {
+            fromPropertyValue = ((ZonedDateTime) fromPropertyValue)
+                    .format(DateTimeFormatter.ofPattern(ISO_8601_COMPATIBLE_DATE_PATTERN));
         } else if (fromPropertyValue instanceof Enum) {
             fromPropertyValue = fromPropertyValue.toString();
         }
