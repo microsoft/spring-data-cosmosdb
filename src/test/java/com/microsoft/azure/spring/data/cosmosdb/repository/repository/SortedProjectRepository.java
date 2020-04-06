b8706c9b0bfe23b3020f6782b32e74eea1f3d39d
Kushagra Thapar <kuthapar@microsoft.com>
2020-04-06 16:54:27 -0700

    Added new Sorted Project repository

---
+++ src/test/java/com/microsoft/azure/spring/data/cosmosdb/repository/repository/SortedProjectRepository.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.spring.data.cosmosdb.repository.repository;
+
+import com.microsoft.azure.spring.data.cosmosdb.domain.SortedProject;
+import com.microsoft.azure.spring.data.cosmosdb.repository.CosmosRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface SortedProjectRepository extends CosmosRepository<SortedProject, String> {
+
+    List<SortedProject> findByNameOrCreator(String name, String creator, Sort sort);
+
+    List<SortedProject> findByNameAndCreator(String name, String creator, Sort sort);
+
+    List<SortedProject> findByForkCount(Long forkCount, Sort sort);
+
+    Page<SortedProject> findByForkCount(Long forkCount, Pageable pageable);
+}
