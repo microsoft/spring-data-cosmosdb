7acfde5db935d4c86aea1a06b313a4fdfae5f73d
Kushagra Thapar <kuthapar@microsoft.com>
2020-07-09 09:06:50 -0700

    Moved spring-data-cosmosdb repo to new repo

--- src/libs/travis-wait-mvn-build.sh
+++
@@ -1,9 +0,0 @@
-function write_visual_bells() {
-  while true; do
-    echo -en "\a"
-    sleep 10
-  done
-}
-write_visual_bells&
-
-set -o pipefail && mvn -P travis-ci-test clean cobertura:cobertura-integration-test | grep -v "DEBUG"
